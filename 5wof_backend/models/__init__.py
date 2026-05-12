a71ff01a8e00340ca98a7d25ce42f3d237f8300b
OlusegunMACS <eeomo007@gmail.com>
2026-05-12 16:14:52 +0100

    remove backend from repository

--- 5wof_backend/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from models.admin import Admin
-from models.blog_post import BlogPost
-from models.contact_submission import ContactSubmission
-from models.event import Event
-
-__all__ = ["Admin", "BlogPost", "ContactSubmission", "Event"]
